09d90c92e106508611fdcad97830a36850da5553
jinsu <jinsu_st@163.com>
2019-02-23 12:42:09 +0800

    code

--- admin.py
+++ admin.py
@@ -11,15 +11,42 @@ from selenium.webdriver.support import expected_conditions as EC
 from PIL import Image
 import pytesseract
 
+import os
+
 pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
 
 url = 'http://admin.kingxin.top'
 
-cr = Chrome(False)
+cr = Chrome(True)
+def save_image(image_url):
+    file_name = 'code.png'
+    cmd = 'wget %s -O %s' %(image_url, file_name)
+    os.system(cmd)
+    return file_name
+
+def image_orc(image_path):
+    im = Image.open(image_path)
+    '''
+    im=im.convert('L')#图片转换为灰色图
+    im=im.convert('RGBA')#图片转换成RGBA模式
+    pixdata = im.load()
+
+    for y in range(im.size[1]):
+	for x in range(im.size[0]):
+	#循环图像里的每一个像素。每个像素为一个长度为4的列表。因为图片转换成RGBA模式，所以列表长度为4，A就是透明度
+	    if pixdata[x,y][0]>170 and pixdata[x,y][1]>170 and pixdata[x,y][2]>170 and pixdata[x,y][3]>170:
+		pixdata[x,y]=(255, 255, 255, 0)
+	    else:  
+		pixdata[x,y]=(0, 0, 0, 0)
+								
+    im.save(image_path)
+    '''
+    code = pytesseract.image_to_string(im,  config="-psm 8 ")
+    print("image code  ===== ", code)
+    return code
 
 def get_webmail_cookie(username, password):
     cr.driver.get(url)
-    wait = WebDriverWait(cr.driver, 10)
     input_name = cr.driver.find_element_by_name('username')
     input_name.send_keys(username)
     input_pw = cr.driver.find_element_by_name('password')
@@ -28,14 +55,12 @@ def get_webmail_cookie(username, password):
     input_captcha = cr.driver.find_element_by_name('captcha')
 
     img = cr.driver.find_element_by_id('captcha')
-    img_link = url + img.get_attribute('src')
-
-    Image = Image.open(img_link)
-    code = pytesseract.image_to_string(Image)
-    print(code)
+    img_link = img.get_attribute('src')
+    img_path = save_image(img_link)
+    code = image_orc(img_path)
     input_captcha.send_keys(code)
 
-    button_login = cr.driver.find_element_by_class_name('mysubmit')
+    button_login = cr.driver.find_element_by_id('mysubmit')
     button_login.click()
 
     wait = WebDriverWait(cr.driver, 10)
@@ -47,8 +72,11 @@ def get_webmail_cookie(username, password):
 
     print(cookies)
 
-get_webmail_cookie("admin", "poiu0987")
-cr.close()
+#image_orc('code.png')
+try:
+    get_webmail_cookie("admin", "poiu0987")
+finally:
+    cr.close()
 
 
 
